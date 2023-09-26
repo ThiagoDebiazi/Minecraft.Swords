22cca9e95574e9dbf322ae48a1b441fea2e81a3a
ThiagoDebiazi <thiagoddebiazi@gmail.com>
2023-09-26 16:23:13 -0300

    reduced the codes and added axe

--- script.js
+++ script.js
@@ -1,45 +1,79 @@
+const danoMachados = {
+    "madeira": 7,
+    "pedra": 9,
+    "ferro": 9,
+    "ouro": 7,
+    "diamante": 9,
+    "netherite": 10,
+}
+const danoEspadas = {
+    "madeira": 4,
+    "pedra": 5,
+    "ferro": 6,
+    "ouro": 4,
+    "diamante": 7,
+    "netherite": 8,
+}
 
-function calcularENotificar() {
+function calcularENotificarE() {
     const espadaSelectRef = document.querySelector('select[name="Espada"]')
-    const encantamentoSelectRef = document.querySelector('select[name="Encantamento"]')
-    const nivelSelectRef = document.querySelector('select[name="Nivel"]')
+    const encantamentoSelectRef = document.querySelector('select[name="EncantamentoEspada"]')
+    const nivelSelectRef = document.querySelector('select[name="NivelEspada"]')
 
 
     const espadaSelecionada = espadaSelectRef.value
     const encantamentoSelecionado = encantamentoSelectRef.value
     const nivelSelecionado = nivelSelectRef.value
 
-    const resultado = calcularDano(espadaSelecionada, encantamentoSelecionado, nivelSelecionado)
-    document.querySelector('.result').innerHTML = resultado
+    const resultado = calcularDanoE(espadaSelecionada, encantamentoSelecionado, nivelSelecionado)
+    document.querySelector('.resultE').innerHTML = resultado
 }
 
-function calcularDano(espada, encantamento, nivel) {
-    let dano = 0
-    if (espada == 'madeira') {
-        dano = 4
-    }
-    if (espada == 'pedra') {
-        dano = 5
-    }
-    if (espada == 'ferro') {
-        dano = 6
-    }
-    if (espada == 'ouro') {
-        dano = 4
-    }
-    if (espada == 'diamante') {
-        dano = 7
-    }
-    if (encantamento == 'afiacao') {
-        dano = dano + (1.25 * nivel)
-    }
-    if (encantamento == 'ruinadosartropodes') {
-        dano = dano + (2.5 * nivel)
-    }
-    if (encantamento == 'julgamento') {
-        dano = dano + (2.5 * nivel)
+function calcularDanoE(espada, encantamento, nivel) {
+    let dano = danoEspadas[espada]
+    switch (encantamento) {
+        case "afiacao":
+            dano = dano + (1.25 * nivel)
+            break;
+        case "ruinadosartropodes":
+            dano = dano + (2.5 * nivel)
+            break;
+        case "julgamento":
+            dano = dano + (2.5 * nivel)
+            break;
+        default: ''
+            break;
     }
     return dano
 }
 
+function calcularENotificarM() {
+    const machadoSelectRef = document.querySelector('select[name="Machado"]')
+    const encantamentoSelectRef = document.querySelector('select[name="EncantamentoMachado"]')
+    const nivelSelectRef = document.querySelector('select[name="NivelMachado"]')
 
+
+    const machadoSelecionado = machadoSelectRef.value
+    const encantamentoSelecionado = encantamentoSelectRef.value
+    const nivelSelecionado = nivelSelectRef.value
+
+    const resultado = calcularDanoM(machadoSelecionado, encantamentoSelecionado, nivelSelecionado)
+    document.querySelector('.resultM').innerHTML = resultado
+}
+function calcularDanoM(machado, encantamento, nivel) {
+    let dano = danoMachados[machado]
+    switch (encantamento) {
+        case "afiacao":
+            dano = dano + (1.25 * nivel)
+            break;
+        case "ruinadosartropodes":
+            dano = dano + (2.5 * nivel)
+            break;
+        case "julgamento":
+            dano = dano + (2.5 * nivel)
+            break;
+        default: ''
+            break;
+    }
+    return dano
+} 
