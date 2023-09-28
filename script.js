85299b9d14ffba281de9147c5cad3563c27669c4
ThiagoDebiazi <thiagoddebiazi@gmail.com>
2023-09-28 17:19:01 -0300

    add some description and started a dps counter

--- script.js
+++ script.js
@@ -15,6 +15,24 @@ const danoEspadas = {
     "netherite": 8,
 }
 
+const dpsEspada = {
+    "madeira": 1.6,
+    "pedra": 1.6,
+    "ferro": 1.6,
+    "ouro": 1.6,
+    "diamante": 1.6,
+    "netherite": 1.6,
+}
+const dpsMachado = {
+    "madeira": 0.8,
+    "pedra": 0.8,
+    "ferro": 0.9,
+    "ouro": 1,
+    "diamante": 1,
+    "netherite": 1,
+}
+
+
 function calcularENotificarE() {
     const espadaSelectRef = document.querySelector('select[name="Espada"]')
     const encantamentoSelectRef = document.querySelector('select[name="EncantamentoEspada"]')
@@ -29,7 +47,7 @@ function calcularENotificarE() {
     document.querySelector('.resultE').innerHTML = resultado
 }
 
-function calcularDanoE(espada, encantamento, nivel) {
+function calcularDanoE(espada, encantamento, nivel, dps) {
     let dano = danoEspadas[espada]
     switch (encantamento) {
         case "afiacao":
@@ -76,4 +94,11 @@ function calcularDanoM(machado, encantamento, nivel) {
             break;
     }
     return dano
-} 
+}
+function calcularDpsM(machado, encantamento, nivel) {
+    return (calcularDanoM(machado, encantamento, nivel) * dpsMachado[machado]).toFixed(2)
+}
+function calcularDpsE(espada, encantamento, nivel) {
+    return (calcularDanoE(espada, encantamento, nivel) * dpsEspada[espada]).toFixed(2)
+}
+console.log(calcularDpsE("madeira", "afiacao", "4"))
